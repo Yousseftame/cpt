058c3e1245e3eb39bb24fda3c69081611e8d4d0b
Yousseftame <tamerosama73@gmail.com>
2025-12-22 15:39:42 +0200

    fix SPA routing on vercel

--- src/Pages/Dashbaord/Dashboard.tsx
+++ src/Pages/Dashbaord/Dashboard.tsx
@@ -1,11 +1,20 @@
 // import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import LogoutButton from '../../components/shared/LogoutButton'
 
 export default function Dashboard() {
+
+
+  const navigate = useNavigate();
   return (
     <div>
-      dashboard
+      <div className="flex justify-between items-center mb-4">
+ dashboard
+   <button   onClick={() => navigate('/customer')}>Customers</button>
+   <button onClick={() => navigate('/requests')}>requests</button>
       <LogoutButton />
+      </div>
+     
     </div>
   )
 }
