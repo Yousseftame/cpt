654be9c922bcceed263164828840c44aa5afc2a9
Yousseftame <tamerosama73@gmail.com>
2025-12-23 11:02:12 +0200

    outlet

--- src/Pages/Dashbaord/Dashboard.tsx
+++ src/Pages/Dashbaord/Dashboard.tsx
@@ -1,19 +1,14 @@
 // import React from 'react'
-import { useNavigate } from 'react-router-dom'
 import LogoutButton from '../../components/shared/LogoutButton'
 
 export default function Dashboard() {
 
 
-  const navigate = useNavigate();
   return (
     <div>
-      <div className="flex justify-between items-center mb-4">
  dashboard
-   <button   onClick={() => navigate('/customer')}>Customers</button>
-   <button onClick={() => navigate('/requests')}>requests</button>
+  
       <LogoutButton />
-      </div>
      
     </div>
   )
--- src/layouts/MasterLayout/MasterLayout.tsx
+++ src/layouts/MasterLayout/MasterLayout.tsx
@@ -1,9 +1,14 @@
 // import React from 'react'
 import { Outlet } from 'react-router-dom'
+import SideBar from '../../components/sidebar/SideBar'
+import NavBar from '../../components/navbar/NavBar'
 
 export default function MasterLayout() {
   return (
     <div>
+      <NavBar/>
+     <SideBar/>
+
         <Outlet />
     </div>
   )
