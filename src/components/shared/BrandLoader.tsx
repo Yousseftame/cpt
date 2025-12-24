678216c11daa9155fa79d1648ae380416e5d20f5
Yousseftame <tamerosama73@gmail.com>
2025-12-24 22:37:55 +0200

    purchased request

--- src/components/shared/BrandLoader.tsx
+++ src/components/shared/BrandLoader.tsx
@@ -5,7 +5,7 @@ export default function AppLoader({
 }) {
   return (
 <div
-  className="p-3 animate-spin drop-shadow-2xl bg-gradient-to-bl from-pink-400 via-purple-400 to-indigo-600 md:w-48 md:h-48 h-32 w-32 aspect-square rounded-full"
+  className="p-3 animate-spin drop-shadow-2xl bg-gradient-to-bl from-pink-400 via-purple-400 to-indigo-600 md:w-28 md:h-28 h-32 w-32 aspect-square rounded-full"
 >
   <div
     className="rounded-full h-full w-full bg-slate-100 background-blur-md"
